f05bf68597aba656b96410269d78663b89f533f2
Amey Naik <48198906+NAmey89@users.noreply.github.com>
2019-08-04 00:45:05 +0530

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,13 @@
+/* const pi = 3.14;
+pi = 3.24;
+console.log(pi); */
+
+// creating object named game
+let game = {
+    pitch:'grass',
+    time:'90 minutes',
+    teams: 2
+};
+
+console.log(game);
+console.log(game.time);
